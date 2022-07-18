4ef468b16cc4147a6ba70308ae1bc7acc2f15f52
FilippoBignami <63307239+FilippoBignami@users.noreply.github.com>
2022-07-18 18:46:38 +0200

    Add files via upload

---
+++ Modulo1/project/Game_FGC.js
@@ -0,0 +1,190 @@
+//Filippo Bignami
+
+//*****************************************************************************************************************
+var  controls = new function(){
+    this.lightIntensity = 2.5;
+   this.shadowIntensity = 0.5
+   this.x_light = 52;
+   this.y_light = 440;
+   this.z_light  = 10;
+  
+  }
+  var canvas = document.getElementById('my_Canvas');
+  var gl = canvas.getContext('webgl');
+  var salpha = 0.0025;
+if (!gl) {
+      console.log("NON SUPPORTA WEBGL Context");
+  }
+  
+  //ext serve per la depth texture, se no diventa tutto nero
+  var ext = gl.getExtension('WEBGL_depth_texture');
+  if (!ext) {
+    console.log("NON SUPPORTA WEBGL_depth_texture");  // eslint-disable-line
+    }	
+  let command1 = "--COMANDI:-- -W : --Avanti--; -S : --Indietro--; -A : --Sinistra--; -D : --Destra--; -----SPACE:----- Raccogli/Apri  ";
+  document.getElementById('Command').innerHTML = command1;
+  let string = "TROVA LE 3 CHIAVI SPARSE NELLA CITTA' E LIBERA LA PRINCIPESSA RINCHIUSA NELLA TORRE!";
+  document.getElementById('text').innerHTML = string;
+  setGeometries(gl);
+
+  var programInfo_scene = webglUtils.createProgramInfo(gl, ["scene-vs", "scene-fs"]);
+  var programInfo_color = webglUtils.createProgramInfo(gl, ["color-vs", "color-fs"]);
+  
+  setTextures();
+  createTextureForLights();
+  setObjsToDraw();
+
+  //-----------------------------------------------------------------
+  // global parameters
+  
+  var matrix_car, matrix_wheels;
+  var changes=false; 
+  var nstep=0; 
+  var timeNow=0;
+
+  const PHYS_SAMPLING_STEP=20; 
+  
+  var Theta_resetta = 1.57;
+  var bias = -0.00005;
+  var dr = 5.0 * Math.PI/180.0;
+  var lightIntensity = 2.5;
+  var shadowIntensity= 0.5;
+  var PHI=0.3482;	//degToRad(86)	
+  var THETA=1.57; 
+  var PHI_2=0.6973;	//degToRad(86)	
+  var THETA_2=3.7517; 	//degToRad(23)
+  var	D=  54; //40,					
+  var	target= [0, 0, 0]; //at dove guardiamo
+  var	up= [0, 1, 0]; //se cambia up, ruota l'intero SDR, quindi cambiano gli assi
+  var	aspect=  gl.canvas.clientWidth / gl.canvas.clientHeight;
+  var	zmin= 1;	// faccia più piccola del frustum znear
+  var	zmax=  100;	// faccia più grande del frustum zfar
+  var	fov=  55; 	// aumentando questo, aumento l'ampiezza della visuale (tipo grandangolo)
+  var	x_light=  52;
+  var	y_light=  440;
+  var	z_light= 10; 
+  var	x_targetlight= 0;	
+  var	y_targetlight=  0;	
+  var	z_targetlight=  0;				
+  var	width_projLight=  30;
+  var	height_projLight=  30;
+  var	fovLight= 500;
+  
+  window.onwheel = e => {
+      if(e.deltaY >= 0){
+      // Scrolling Down with mouse
+      D *=1.1; 
+      
+      render();
+      
+      } else {
+      // Scrolling Up with mouse
+      D *=0.9; 
+      
+      render();
+      }
+  }
+  
+  var drag;
+  var gc_btnw=10, gc_btnh=4;
+  var btnw=gc_btnw.toString()+"%", btnh=gc_btnh.toString()+"%";
+  btn("ButtonIncreaseD", "90%", "25%", "8%", btnh, function(){D  *= 1.1;}, "INCREASE_D", "lightgreen", "");
+  btn("ButtonDecreaseD", "90%", "30%", "8%", btnh, function(){D *= 0.9;}, "DECREASE_D", "lightgreen", "");
+  btn("ButtonViewRetro", "89%", "55%", "10%", "6%", function(){Swap_camera = !Swap_camera;}, "SWAP CAMERA", "lightred", "");
+  btn("ButtonIncreaseTheta", "89%", "35%", "10%", btnh,  function(){THETA += dr;}, "INCREASE_THETA", "red", "");
+  btn("ButtonDecreaseTheta", "89%", "40%", "10%", btnh,  function(){THETA -= dr;}, "DECREASE_THETA", "red", "");
+  btn("ButtonIncreasePhi", "89%", "45%", "10%", btnh,  function(){PHI += dr;}, "INCREASE_PHI", "lightblue", "");
+  btn("ButtonDecreasePhi", "89%", "50%", "10%", btnh,  function(){PHI-= dr;}, "DECREASE_PHI", "lightblue", "");
+  btn("ResetCamera", "89%", "63%", "10%", "6%", function(){reset_camera = true;THETA=Theta_resetta;}, "RESET  CAMERA", "lightred", "");
+
+  if( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
+    var gui = new dat.GUI();
+  
+    gui.add(controls,'lightIntensity',0,10);
+    gui.add(controls,'shadowIntensity',0,4);
+    gui.add(controls,'x_light',0,300);
+    gui.add(controls,'y_light',300,600);
+    gui.add(controls,'z_light',0,100);
+
+    if (screen.width === 1920 && screen.height ===1080)
+        document.getElementById("my_Canvas").style.width = "1400px";
+
+    document.getElementById("ButtonA").style.visibility = "hidden";
+    document.getElementById("ButtonW").style.visibility = "hidden";
+    document.getElementById("ButtonS").style.visibility = "hidden";
+    document.getElementById("ButtonD").style.visibility = "hidden";
+    document.getElementById("ButtonCatch").style.visibility = "hidden";
+          
+  }
+  else { //i'm in the mobile version
+      document.getElementById("my_Canvas").style.height = "320px";
+      document.getElementById("my_Canvas").style.width = "680px";
+      document.getElementById("my_Canvas").style.left = "50px";
+      document.getElementById("my_Canvas").style.top = "50px";
+      document.getElementById("container_text").style.top = "15px";
+      document.getElementById("container_text").style.left = "450px";
+      document.getElementById("container_text").style.width = "500px";
+
+      document.getElementById("ButtonIncreaseD").style.top = "400px";
+      document.getElementById("ButtonIncreaseD").style.left = "100px";
+    
+
+      document.getElementById("ButtonDecreaseD").style.top = "440px";
+      document.getElementById("ButtonDecreaseD").style.left = "100px";
+   
+
+      document.getElementById("ButtonIncreaseTheta").style.top = "400px";
+      document.getElementById("ButtonIncreaseTheta").style.left = "250px";
+      
+
+      document.getElementById("ButtonDecreaseTheta").style.top = "440px";
+      document.getElementById("ButtonDecreaseTheta").style.left = "250px";
+    
+
+      document.getElementById("ButtonIncreasePhi").style.top = "400px";
+      document.getElementById("ButtonIncreasePhi").style.left = "400px";
+      
+
+      document.getElementById("ButtonDecreasePhi").style.top = "440px";
+      document.getElementById("ButtonDecreasePhi").style.left = "400px";
+     
+
+      document.getElementById("ButtonViewRetro").style.top = "400px";
+      document.getElementById("ButtonViewRetro").style.left = "550px";
+      
+
+      document.getElementById("ResetCamera").style.top = "440px";
+      document.getElementById("ResetCamera").style.left = "550px";
+     
+      bias = -0.005; //su mobile lo metto diverso, se no viene tutto nero
+  }
+
+
+  // -----------------------------------------------------------------
+  // actions for mouse, keyboard and mobile
+  
+
+  document.getElementById("ButtonW").ontouchstart = function(){key[0] = true; };
+  document.getElementById("ButtonW").ontouchend = function(){key[0] = false; };
+
+  document.getElementById("ButtonS").ontouchstart = function(){key[2]=true; };
+  document.getElementById("ButtonS").ontouchend = function(){key[2]=false; };
+
+  document.getElementById("ButtonA").ontouchstart = function(){key[1] = true; };
+  document.getElementById("ButtonA").ontouchend = function(){key[1] = false; };
+
+  document.getElementById("ButtonD").ontouchstart = function(){key[3] = true; };
+  document.getElementById("ButtonD").ontouchend = function(){key[3] = false; };
+
+  document.getElementById("ButtonCatch").onclick = function(){key[4]=true;};
+  canvas.onmousedown = mouseDown;
+  canvas.onmouseup = mouseUp;
+  canvas.mouseout = mouseUp;
+  canvas.onmousemove = mouseMove;
+  window.addEventListener('keydown', doKeyDown, true);
+  window.addEventListener('keyup', doKeyUp, true);
+              
+  CarInit(); 
+  update(); // start animation
+  window.requestAnimationFrame(update);
+
---
+++ Modulo1/project/SetGeometries.js
@@ -0,0 +1,592 @@
+//Filippo Bignami
+
+var objectsToDraw = [];
+
+function setObjsToDraw() {
+	objectsToDraw = [
+		
+		{
+			name: "floor",
+			bufferInfo: bufferInfo_floor,
+			uniforms: {
+				u_colorMult: [0.5, 0.5, 1, 1],
+				u_texture: textures[0],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			name: "fontana",
+			bufferInfo: bufferInfo_obj_fontana,
+			uniforms: {
+				
+				u_texture: textures[1],
+				u_world: m4.scale(m4.translation(-300, 0, 50), 15, 15, 15),
+			},
+		},
+
+{
+			name: "paretedx",
+			bufferInfo: bufferInfo_paretedx,
+			uniforms: {
+				u_texture: textures[2],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			name: "paretesx",
+			bufferInfo: bufferInfo_paretesx,
+			uniforms: {
+				u_texture: textures[2],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			name: "back",
+			bufferInfo: bufferInfo_pareteback,
+			uniforms: {
+				u_texture: textures[3],
+				u_world: m4.identity(),
+			},
+		},
+
+		{
+			name: "front",
+			bufferInfo: bufferInfo_paretefront,
+			uniforms: {
+				u_texture: textures[3],
+				u_world: m4.identity(),
+			},
+		},
+
+		{
+			name: "Key1",
+			bufferInfo: bufferInfo_obj_Key,
+			uniforms: {
+				
+				u_texture: textures[4],
+				u_world: m4.scale(m4.yRotate(m4.translation(600, 2, 250),degToRad(-90)), 3, 3,3),
+						
+			},
+		},
+
+		{
+			name: "Key2",
+			bufferInfo: bufferInfo_obj_Key,
+			uniforms: {
+				
+				u_texture: textures[4],
+				u_world: m4.scale(m4.yRotate(m4.translation(330, 2, 286),degToRad(-90)), 3, 3,3),
+						
+			},
+		},
+
+		{
+			name: "Key3",
+			bufferInfo: bufferInfo_obj_Key,
+			uniforms: {
+				
+				u_texture: textures[4],
+				u_world: m4.scale(m4.translation(30, 2, 60), 3, 3,3)
+						
+			},
+		},
+		{
+			//not affected by the light
+			name: "Area1",
+			bufferInfo: bufferInfo_keyarea,
+			uniforms: {
+				u_texture: textures[18],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			//not affected by the light
+			name: "Area2",
+			
+			bufferInfo: bufferInfo_keyarea,
+			uniforms: {
+				u_texture: textures[18],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			//not affected by the light
+			name: "Area3",
+			
+			bufferInfo: bufferInfo_keyarea,
+			uniforms: {
+				u_texture: textures[18],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			
+			name: "Area4",
+			
+			bufferInfo: bufferInfo_keyarea,
+			uniforms: {
+				u_texture: textures[18],
+				u_world: m4.identity(),
+			},
+		},
+
+		{
+			
+			name: "Princess",
+			
+			bufferInfo: bufferInfo_Princess,
+			uniforms: {
+				u_texture: textures[6],
+				u_world: m4.identity(),
+			},
+		},
+
+		{
+			name: "Albero1",
+			bufferInfo: bufferInfo_obj_tree,
+			uniforms: {
+				
+				u_texture: textures[7],
+				u_world: m4.scale(m4.translation(-260, 0, 50), 15, 15, 15),
+			},
+		},
+
+		{
+			name: "Albero2",
+			bufferInfo: bufferInfo_obj_tree,
+			uniforms: {
+				
+				u_texture: textures[7],
+				u_world: m4.scale(m4.translation(460, 0, 150), 12, 12, 12),
+			},
+		},
+
+
+		{
+			name: "Albero3",
+			bufferInfo: bufferInfo_obj_tree,
+			uniforms: {
+				
+				u_texture: textures[7],
+				u_world: m4.scale(m4.translation(400, 0, -100), 17, 17, 17),
+			},
+		},
+		{
+			name: "Welcome",
+			bufferInfo: bufferInfo_Welcome,
+			uniforms: {
+				u_texture: textures[8],
+				u_world: m4.identity(),
+			},
+		},
+		{
+			name: "Torre",
+			bufferInfo: bufferInfo_obj_Torre,
+			uniforms: {
+				
+				u_texture: textures[9],
+				u_world: m4.scale(m4.translation(-550, 30, 600), 50, 50, 50),
+						
+			},
+		},
+
+		{
+			name: "Edificio1",
+			bufferInfo: bufferInfo_obj_grattacielo,
+			uniforms: {
+				
+				u_texture: textures[10],
+				u_world: m4.scale(m4.translation(-150, -30, -200), 4, 4, 4),
+						
+			},
+		},
+		{
+			name: "Edificio2",
+			bufferInfo: bufferInfo_obj_grattacielo,
+			uniforms: {
+				
+				u_texture: textures[11],
+				u_world: m4.scale(m4.translation(100, -30, -200), 4, 4, 4),
+						
+			},
+		},
+
+		{
+			name: "capanna",
+			bufferInfo: bufferInfo_obj_capanna,
+			uniforms: {
+				
+				u_texture: textures[4],
+				u_world: m4.scale(m4.translation(600, 0, 100), 20, 20, 20),
+						
+			},
+		},
+		{
+			name: "David",
+			bufferInfo: bufferInfo_obj_David,
+			uniforms: {
+				
+				u_texture: textures[12],
+				u_world: m4.scale(m4.xRotate(m4.yRotate(m4.translation(300, 0, 370),degToRad(180)),degToRad(-90)), 0.2, 0.2, 0.2),
+						
+			},
+		},
+
+		//automobile
+		{
+			name: "extra",
+			bufferInfo: bufferInfo_obj_chassis,
+			uniforms: {
+				u_texture: textures[13],
+				u_world: m4.scale(m4.translation(1, 5, 0), 5, 5, 5),
+			},
+		},
+		{
+			name: "fari",
+			bufferInfo: bufferInfo_objfari,
+			uniforms: {
+				u_texture: textures[14],
+				u_world: m4.scale(m4.translation(1, 5, 0), 5, 5, 5),
+			},
+		},
+		{
+			name: "carrozzeria",
+			bufferInfo: bufferInfo_obj_chassisnowind,
+			uniforms: {
+				u_diffuse: [1, 1, 1, 1],
+				u_texture: textures[5],
+				//u_world: m4.identity(),
+				u_world: m4.scale(m4.translation(1, 5, 0), 5, 5, 5),
+			},
+		},
+		{
+			name: "windows",
+			bufferInfo: bufferInfo_obj_ch,
+			uniforms: {
+				u_diffuse: [1, 1, 1, 1],
+				u_texture: textures[15],
+				//u_world: m4.identity(),
+				u_world: m4.scale(m4.translation(1, 5, 0), 5, 5, 5),
+			},
+		},
+		{
+			name: "wheel_adx",
+			bufferInfo: bufferInfo_obj_wheel,
+			uniforms: {
+				u_texture: textures[16],
+				u_world: m4.scale(m4.translation(6, 2, -2), .6, .6, .6),
+			},
+		},
+		{
+			name: "wheel_asx",
+			bufferInfo: bufferInfo_obj_wheel,
+			uniforms: {
+				u_texture: textures[16],
+				u_world: m4.scale(m4.translation(-6, 2, -2), .6, .6, .6),
+			},
+		},
+		{
+			name: "wheel_pdx",
+			bufferInfo: bufferInfo_obj_wheel,
+			uniforms: {
+				u_texture: textures[16],
+				u_world: m4.identity(), //m4.scale(m4.translation(6, 2, 18), .6, .6, .6),
+			},
+		},
+		{
+			name: "wheel_psx",
+			bufferInfo: bufferInfo_obj_wheel,
+			uniforms: {
+				u_texture: textures[16],
+				u_world: m4.identity(),
+			},
+		},
+	];
+}
+
+
+// ****************************************************************************************************************
+// GEOMETRIES
+// ****************************************************************************************************************
+
+function setGeometries(gl) {
+
+// ---------------------------------------------------------------------
+	//FRUSTUM
+	cubeLinesBufferInfo = webglUtils.createBufferInfoFromArrays(gl, {
+		position: [	-1, -1, -1,	1, -1, -1,	-1,  1, -1,	1,  1, -1,	-1, -1,  1,	 1, -1,  1,	-1,  1,  1,	 1,  1,  1,	],
+		indices: [	0, 1,	1, 3,	3, 2,	2, 0,
+					4, 5,	 5, 7,	 7, 6,	 6, 4,
+					0, 4,	 1, 5,	  3, 7,	  2, 6,	], 
+	});
+
+	// ---------------------------------------------------------------------
+	// plane
+	{
+		const S = 10; 		
+		const H = 0.02; 
+
+		const textureCoords = [ 0,0, 1,0, 0,1, 1,1,];
+
+		const arrays_floor = {
+		   position: 	{ numComponents: 3, data: [-S-10,H,-S, S+5,H,-S, -S-10,H,S,  S+5,H,S, ], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_floor = webglUtils.createBufferInfoFromArrays(gl, arrays_floor);
+	}
+
+
+	{
+		const S = 10; 		
+		const H = 5.0; 
+
+		const textureCoords = [0,0, 1,0, 0,1, 1,1,];
+
+		const arrays_paretedx = {
+		   position: 	{ numComponents: 3, data: [-S-10,H+4.40,-S, S+5,H+4.40,-S, -S-10,H-5,-S, S+5,H-5,-S,  ], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_paretedx = webglUtils.createBufferInfoFromArrays(gl, arrays_paretedx);
+	}
+	{
+		const S = 10; 		
+		const H = 5.0; 
+
+		const textureCoords = [  0,1, 1,1, 0,0, 1,0,];
+
+		const arrays_paretesx = {
+		   position: 	{ numComponents: 3, data: [S+5,H-5,S, -S-10,H-5,S, S+5,H+4.40,S, -S-10,H+4.40,S,  ], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_paretesx = webglUtils.createBufferInfoFromArrays(gl, arrays_paretesx);
+	}
+	{
+		const S = 10; 		
+		const H = 5.0; 
+
+		const textureCoords = [  0,1, 1,1, 0,0, 1,0,];
+		
+		const arrays_back = {
+		   position: 	{ numComponents: 3, data: [-S-10,H-5,S, -S-10,H-5 ,-S,-S-10,H+4.40,S, -S-10,H+4.40,-S, ], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_pareteback = webglUtils.createBufferInfoFromArrays(gl, arrays_back);
+	}
+	{
+		const S = 10; 		
+		const H = 5.0; 
+
+		const textureCoords = [  0,1, 1,1, 0,0, 1,0,];
+		
+		const arrays_front = {
+		   position: 	{ numComponents: 3, data:  [S+5,H-5,-S, S+5,H-5,S, S+5,H+4.40,-S, S+5,H+4.40,S, ], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_paretefront = webglUtils.createBufferInfoFromArrays(gl, arrays_front);
+	}
+
+
+	{
+		const S = 5; 		
+		const H = 3; 
+
+		const textureCoords = [  0,1, 1,1, 0,0, 1,0,];
+		
+		const arrays_welcome = {
+		   position: 	{ numComponents: 3, data: [S+5,H-5,-S, S+5,H-5,S, S+5,H+4.40,-S, S+5,H+4.40,S,], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_Welcome = webglUtils.createBufferInfoFromArrays(gl, arrays_welcome);
+	}
+
+	{
+		const S = 5; 		
+		const H = 1; 
+
+		const textureCoords = [  0,1, 1,1, 0,0, 1,0,,];
+		
+		const arrays_princess = {
+		   position: 	{ numComponents: 3, data: [S+5,H-5,-S, S+5,H-5,S, S+5,H+4.40,-S, S+5,H+4.40,S,], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   //color: 	{ numComponents: 3, data: [0.7,0.7,0.7,  0.7,0.7,0.7,  0.7,0.7,0.7,  0.7,0.7,0.7], },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_Princess= webglUtils.createBufferInfoFromArrays(gl, arrays_princess);
+	}
+
+
+
+	{
+		const S = 10; 		
+		const H = 0; 
+
+		const textureCoords = [ 0,0, 1,0, 0,1, 1,1,];
+
+		const arrays_keyarea = {
+		   position: 	{ numComponents: 3, data: [-S,H,-S, S,H,-S, -S,H,S,  S,H,S, ], },
+		   texcoord: 	{ numComponents: 2, data: textureCoords, },
+		   //color: 	{ numComponents: 3, data: [0.7,0.7,0.7,  0.7,0.7,0.7,  0.7,0.7,0.7,  0.7,0.7,0.7], },
+		   indices: 	{ numComponents: 3, data: [0,2,1, 	2,3,1,], },
+		   normal:		{numComponents: 3, data: [0,1,0,	0,1,0,	0,1,0,	0,1,0,], },
+		};
+
+		bufferInfo_keyarea = webglUtils.createBufferInfoFromArrays(gl, arrays_keyarea);
+	}
+
+	// ****************************************************************************************************************
+	// LOAD FILE.OBJ 
+	// ****************************************************************************************************************
+
+	// ---------------------------------------------------------------------
+
+
+	loadDoc('resources/data/fontana.obj');
+
+	const arrays_obj = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_fontana = webglUtils.createBufferInfoFromArrays(gl, arrays_obj);
+
+	loadDoc('resources/data/chiave.obj');
+
+	const arrays_obj_key = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_Key = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_key); 
+
+
+	loadDoc('resources/data/albero1.obj');
+
+	const arrays_obj_tree = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_tree = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_tree); 
+
+
+	loadDoc('resources/data/torre.obj');
+
+	const arrays_obj_Torre = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_Torre = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_Torre);
+
+
+	loadDoc('resources/data/edificio1.obj');
+
+	const arrays_obj_grattacielo = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_grattacielo = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_grattacielo);
+
+	loadDoc('resources/data/capanna.obj');
+
+	const arrays_obj_capanna = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_capanna = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_capanna);
+
+	loadDoc('resources/data/David.obj');
+
+	const arrays_obj_david = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_David = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_david);
+
+	// ---------------------------------------------------------------------
+	// wheels
+
+	loadDoc('resources/data/ruota.obj');
+
+	const arrays_obj_wheel = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+	bufferInfo_obj_wheel = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_wheel);
+ 
+	// ---------------------------------------------------------------------
+	// car
+	loadDoc('resources/data/windows.obj');
+
+	const arrays_obj_ch = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_ch = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_ch);
+	
+	loadDoc('resources/data/car_without_wind.obj');
+
+	const arrays_obj_chassisnowind = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_chassisnowind = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_chassisnowind);
+	
+	loadDoc('resources/data/extra.obj');
+
+	const arrays_obj_chassis = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_obj_chassis = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_chassis);
+	loadDoc('resources/data/fari.obj');
+
+	const arrays_obj_fari = {
+	   position:	{ numComponents: 3, data:webglVertexData[0], },
+	   texcoord:	{ numComponents: 2, data:webglVertexData[1], },
+	   normal:		{ numComponents: 3, data:webglVertexData[2], },
+	};
+
+	bufferInfo_objfari = webglUtils.createBufferInfoFromArrays(gl, arrays_obj_fari);
+	
+
+	
+
+}
